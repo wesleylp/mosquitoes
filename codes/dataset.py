7289c980b2d119319f7b24e5f4b9b3635f16660d
Wesley L Passos <33898112+wesleylp@users.noreply.github.com>
2018-10-27 10:12:35 -0300

    Dataset preparation (#10)
    * added get_all_frames to video_handling
    
    * added dataSet class
    
    * added get annotation by frame
    
    * refactored video_handling.py -- using get_annotation
    
    * added files utils
    
    * added dataloader
    
    * added init module
    
    * Working on dataloader
    
    * added notebook showing how to use Directory class

---
+++ codes/dataset.py
@@ -0,0 +1,37 @@
+from utils.files_utils import Directory
+from utils.vid_utils import find_annot_file
+from video_handling import videoObj
+
+
+class VideoDataset:
+    def __init__(self, root_dir, annotation_folder=None, transform=None):
+        ext = ('.mp4', '.mov')
+        self.root_dir = root_dir
+        self.video_list = Directory.get_files(self.root_dir, ext, recursive=True)
+        self.annotation_folder = annotation_folder
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.video_list)
+
+    def __getitem__(self, idx):
+
+        annot_path = None
+        if self.annotation_folder is not None:
+            # look for annotation file
+            annot_path = find_annot_file(self.video_list[idx], self.annotation_folder)
+
+        vid = videoObj(self.video_list[idx], annot_path)
+
+        frames = vid.get_all_frames()
+        annot = vid.get_annotations()
+
+        if self.transform:
+            frames = self.transform(frames)
+
+        bboxes = [bbox for bbox in annot.annotation_dict.values()]
+        bboxes = annot.annotation_dict
+
+        sample = {'frames': frames, 'bboxes': bboxes}
+
+        return sample
