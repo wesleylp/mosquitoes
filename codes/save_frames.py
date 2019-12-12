df1f610b2d35769e94f036efc83a04df29d6e2be
Wesley Passos <wesley.passos@smt.ufrj.br>
2019-12-12 16:13:06 -0300

    add .avi and change default folder

--- codes/save_frames.py
+++ codes/save_frames.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--datapath',
         type=str,
-        default='../data/DJI4_cam/2018-09-05/seq001',
+        default='../data/dataset/_under_construction',
         help='Data path of videos to extract frames from.')
 
     parser.add_argument('--save_every', type=int, default=50, help='Number of frames to skip.')
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 
         for filename in filenames:
 
-            if filename.lower().endswith(('.mov', '.mp4')):
+            if filename.lower().endswith(('.mov', '.mp4', '.avi')):
 
                 input_path = os.path.join(dirpath, filename)
                 output_path = os.path.join(dirpath, os.path.splitext(filename)[0])
