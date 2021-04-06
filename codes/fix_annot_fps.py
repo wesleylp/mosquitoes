37e0cee54fb851b45623c605ea6cb54fc0237831
Wesley Passos <wesley.passos@smt.ufrj.br>
2021-04-06 08:58:45 -0300

    add fix annoations fps add code to automaticly fix the annoations when changed the videos fps to 24fps

---
+++ codes/fix_annot_fps.py
@@ -0,0 +1,41 @@
+import xml.etree.ElementTree as ET
+from math import ceil
+"""
+This code was used to fix annotations when we changed the videos fps to 24fps.
+"""
+
+video_name = '20181112_rectified_DJI_0043'
+
+filename = f'/home/wesley/Downloads/anotacao/{video_name}.xml'
+
+tree = ET.parse(filename)
+root = tree.getroot()
+
+stop_frame = int(tree.find('.//stop_frame').text)
+tree.find('.//stop_frame').text = str(round(stop_frame / 2))
+
+tracks = root.findall('track')
+
+# iterate over tracks
+for track in tracks:
+    boxes = track.findall('box')
+
+    # iterate over bboxes
+    for box in boxes:
+        info = box.attrib
+        frame = int(info['frame'])
+
+        if frame % 2 == 0 and frame != 0:
+            track.remove(box)
+
+        else:
+            if frame == 1:
+                continue
+            box.set("frame", f"{int(ceil(frame/2))}")
+
+    # set the last one as 1
+    boxes = track.findall('box')
+    boxes[-1].set("outside", "1")
+
+tree.write(
+    f'{video_name}_fps.xml', encoding="utf-8", xml_declaration=True, short_empty_elements=False)
